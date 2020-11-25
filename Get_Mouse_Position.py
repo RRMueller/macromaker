81a740b4f4ef03826ccb518f3a267b6384ff5871
Quinten Lisowe <quinten.lisowe@thelisowe.com>
2020-11-25 16:16:54 -0600

    Added files to be fixed

--- Get_Mouse_Position.py
+++ Get_Mouse_Position.py
@@ -1,33 +1,24 @@
 from pynput import mouse
-from pynput.mouse import Button, Controller
 
-input = ""
-
-'''
-I have no clue how this works but it works
-'''
 
 def on_move(x, y):
-    print('Pointer moved to {0}'.format(
-        (x, y)))
+    print("Pointer moved to {0}".format((x, y)))
+
 
 def on_click(x, y, button, pressed):
-    print('{0} at {1}'.format('Pressed' if pressed else 'Released', (x, y)))
-    '''
+    print("{0} at {1}".format("Pressed" if pressed else "Released", (x, y)))
+    """
     comment out the next 3 lines to disable turn_off_on_click. Only run in python console or IDLE, not Atom or otherwise. Lags to all hell.
-    '''
+    """
     # if not pressed:
     # # Stop listener
     #     return False
 
+
 def on_scroll(x, y, dx, dy):
-    print('Scrolled {0} at {1}'.format(
-        'down' if dy < 0 else 'up',
-        (x, y)))
+    print("Scrolled {0} at {1}".format("down" if dy < 0 else "up", (x, y)))
+
 
 # Collect events until released
-with mouse.Listener(
-        on_move=on_move,
-        on_click=on_click,
-        on_scroll=on_scroll) as listener:
-    listener.join();
+with mouse.Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as listener:
+    listener.join()
--- Macro_Maker.py
+++ Macro_Maker.py
@@ -1,10 +1,4 @@
-from pynput.mouse import Button, Controller
-import keyboard
-import time
-import pyperclip
-import csv
-
-'''
+"""
 This general purpose script is meant to help speed up the repetitiveness of everyday tasks. It operates in the background allowing you to have any amount of windows stacked on top of it while still allowing it to operate. The delays are necessary but not optimal due to how fast the programs this script is interacting with can operate.
 
 Controls:
@@ -24,14 +18,21 @@ keyboard.write('string of text')                                    # Type a str
 data = pyperclip.paste()                                            # Copies the clipboard to variable data
 time.sleep(20)                                                      # Pause for 20 seconds
 time.perfcounter()                                                  # The current time, useful for time/iteration analysis
-'''
+"""
+import time
 
-loop_count=1
+import keyboard
+from pynput.mouse import Button
+from pynput.mouse import Controller
+
+
+loop_count = 1
 buttons = {"left": Button.left, "right": Button.right}
-FastTimeDelay=0.25
-SlowTimeDelay=0.5
+FastTimeDelay = 0.25
+SlowTimeDelay = 0.5
 
-#Clicks a location, copies, clicks another location, pastes
+# Clicks a location, copies, clicks another location, pastes
+
 
 def click_drag_and_release(xpos1, ypos1, xpos2, ypos2, side="left"):
     mouse = Controller()
@@ -39,6 +40,7 @@ def click_drag_and_release(xpos1, ypos1, xpos2, ypos2, side="left"):
     mouse.press(buttons[side])
     mouse.position = (xpos2, ypos2)
     mouse.release(buttons[side])
+
 
 # Clicks a location twice
 def double_click(x, y, side="left"):
@@ -51,16 +53,17 @@ def double_click(x, y, side="left"):
     mouse.press(buttons[side])
     time.sleep(0.1)
     mouse.release(buttons[side])
-    
-def copy_paste(xpos1, ypos1, xpos2, ypos2):
-    mouse = Controller()
-    click(xpos1, ypos1, left)
-    keyboard.send('ctrl+c')
-    time.sleep(FastTimeDelay)
-    click(xpos2, ypos2, right)
-    keyboard.send('ctrl+v')
 
-#Clicks a location
+
+def copy_paste(xpos1, ypos1, xpos2, ypos2):
+    click(xpos1, ypos1)
+    keyboard.send("ctrl+c")
+    time.sleep(FastTimeDelay)
+    click(xpos2, ypos2)
+    keyboard.send("ctrl+v")
+
+
+# Clicks a location
 def click(x, y, side="left"):
     mouse = Controller()
     mouse.position = (x, y)
@@ -68,63 +71,69 @@ def click(x, y, side="left"):
     time.sleep(FastTimeDelay)
     mouse.release(buttons[side])
 
-#Saves
+
+# Saves
 def save():
     keyboard.send("ctrl+s")
 
-#Copies
+
+# Copies
 def copy():
     keyboard.send("ctrl+c")
 
-#Pastes
+
+# Pastes
 def paste():
     keyboard.send("ctrl+v")
 
-#Goes left 1 page
+
+# Goes left 1 page
 def page_left():
     click(440, 60)
 
-#Goes Right 1 page
+
+# Goes Right 1 page
 def page_right():
     click(460, 60)
 
-#Activates when ↑ is pressed
+
+# Activates when ↑ is pressed
 def up_arrow():
-    print('Up key pressed')
+    print("Up key pressed")
     time.sleep(0.25)
-    mouse = Controller()
     global left_current_part
     global right_current_part
 
-#Activates when ↓ is pressed
+
+# Activates when ↓ is pressed
 def down_arrow():
     print("Down key pressed")
     time.sleep(0.25)
-    mouse = Controller()
     global left_current_part
     global right_current_part
 
-#Activates when ← is pressed
+
+# Activates when ← is pressed
 def left_arrow():
-    print('Left key pressed')
+    print("Left key pressed")
     time.sleep(0.25)
-    mouse = Controller()
     global left_current_part
     global right_current_part
 
-#Activates when → is pressed
+
+# Activates when → is pressed
 def right_arrow():
-    print('Right key pressed')
+    print("Right key pressed")
     time.sleep(0.25)
-    mouse = Controller()
     global left_current_part
     global right_current_part
 
-#Continuous loop that listens for keyboard input
+
+# Continuous loop that listens for keyboard input
 while True:
     try:
-        #Up Arrow
-        if keyboard.is_pressed('up'):
+        # Up Arrow
+        if keyboard.is_pressed("up"):
             start_time = time.perf_counter()
             if loop_count > 1:
                 for i in range(loop_count):
@@ -140,8 +149,8 @@ while True:
                 print(elapsed_time)
             print("-----------------------------------")
 
-        #Down Arrow
-        elif keyboard.is_pressed('down'):
+        # Down Arrow
+        elif keyboard.is_pressed("down"):
             start_time = time.perf_counter()
             if loop_count > 1:
                 for i in range(loop_count):
@@ -157,8 +166,8 @@ while True:
                 print(elapsed_time)
             print("-----------------------------------")
 
-        #Left Arrow
-        elif keyboard.is_pressed('left'):
+        # Left Arrow
+        elif keyboard.is_pressed("left"):
             start_time = time.perf_counter()
             if loop_count > 1:
                 for i in range(loop_count):
@@ -174,8 +183,8 @@ while True:
                 print(elapsed_time)
             print("-----------------------------------")
 
-        #Right Arrow
-        elif keyboard.is_pressed('right'):
+        # Right Arrow
+        elif keyboard.is_pressed("right"):
             start_time = time.perf_counter()
             if loop_count > 1:
                 for i in range(loop_count):
@@ -191,7 +200,7 @@ while True:
                 print(elapsed_time)
             print("-----------------------------------")
 
-        elif keyboard.is_pressed('esc'):
+        elif keyboard.is_pressed("esc"):
             break
-    except:
+    except Exception:
         pass
